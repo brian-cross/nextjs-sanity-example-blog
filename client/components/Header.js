77886061b9d45f2eb8ff7e6640a4cc31c36564d0
Brian Cross <brian.cross.1977@gmail.com>
2021-09-21 13:09:26 -0600

    Created Header and Hero components

---
+++ client/components/Header.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </header>
+  );
+}
---
+++ client/components/Hero.js
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1>Hello Blog</h1>
+      <p>
+        An example blog powered by a user friendly content management system
+      </p>
+    </div>
+  );
+}
