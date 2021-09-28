c749ec945f67aaaba9f1b79d3419e45b03aff5f1
Brian Cross <brian.cross.1977@gmail.com>
2021-09-28 07:52:07 -0600

    Style and structure changes

--- client/pages/posts/[slug].js
+++ client/pages/posts/[slug].js
@@ -1,34 +1,97 @@
 import Image from "next/image";
 import BlockContent from "@sanity/block-content-to-react";
+import formatIsoDate from "../../lib/formatIsoDate";
 import { sanityClient } from "../../lib/sanity.server";
 import { urlForImage } from "../../lib/sanity";
 import { groq } from "next-sanity";
 
 export default function Post({ post }) {
-  const serializers = {
-    types: {
-      h1: props => {
-        <h1 style={{ color: "red" }}>{props.node.h1}</h1>;
-      },
-    },
-  };
+  // const serializers = {
+  //   types: {
+  //     code: props => {
+  //       return (
+  //         <pre>
+  //           <code>{props.node.code}</code>
+  //         </pre>
+  //       );
+  //     },
+  //     block: props => {
+  //       return props.node.style === "h1" ? (
+  //         <h1 style={{ color: "red" }}>{props.children}</h1>
+  //       ) : null;
+  //     },
+  //   },
+  // };
+
+  console.log(post.author.bio);
 
   return (
     <>
-      <h2>{post.title}</h2>
-      <h3>{post.subtitle}</h3>
-      <p>{post._createdAt}</p>
-      {post.authors.map((author, index) => (
-        <p key={index}>{author}</p>
-      ))}
-      <p>{post.body}</p>
-      <BlockContent blocks={post.richText} serializers={serializers} />
-      <Image
-        width={600}
-        height={400}
-        src={urlForImage(post.image).width(600).height(400).url()}
-        alt={post.image.altText}
-      />
+      <div className="container reading-column">
+        <h1 className="font-size-xl-fluid">{post?.title}</h1>
+        <h2 className="font-size-md-fluid">{post?.subtitle}</h2>
+        <p>{post?.publishedDate && formatIsoDate(post.publishedDate)}</p>
+        <div className="author">
+          <div className="avatar">
+            {post?.author?.avatar && (
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={urlForImage(post.author.avatar).url()}
+                alt=""
+              />
+            )}
+          </div>
+          <span>{post?.author?.name}</span>
+        </div>
+        <div className="image">
+          {post?.mainImage && (
+            <Image
+              layout="fill"
+              objectFit="cover"
+              src={urlForImage(post.mainImage).url()}
+              alt={post.mainImage?.altText}
+            />
+          )}
+        </div>
+        <BlockContent
+          projectId="3rxiqh2z"
+          dataset="production"
+          blocks={post?.body}
+          // serializers={serializers}
+        />
+      </div>
+      <style jsx>{`
+        .reading-column {
+          max-width: 50rem;
+        }
+
+        h2 {
+          color: #666;
+          font-weight: normal;
+        }
+
+        .image {
+          position: relative;
+          height: 50vw;
+          max-height: 30rem;
+          margin: 2rem 0;
+        }
+
+        .author {
+          display: flex;
+          align-items: center;
+          gap: 1rem;
+        }
+
+        .avatar {
+          position: relative;
+          overflow: hidden;
+          height: 3rem;
+          width: 3rem;
+          border-radius: 1.5rem;
+        }
+      `}</style>
     </>
   );
 }
@@ -51,14 +114,13 @@ export async function getStaticProps({ params }) {
   const query = groq`
   *[_type == "post" && (slug.current == "${params.slug}")]{
     _id,
-    _createdAt,
     title,
     subtitle,
+    author->{name, avatar, bio},
+    mainImage,
+    publishedDate,
     body,
-    image,
-    richText,
     "slug": slug.current,
-    "authors": authors[]->name
   }
   `;
 
