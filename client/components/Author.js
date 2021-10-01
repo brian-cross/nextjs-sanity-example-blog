045f36770866bc2b5b8820891557698129114386
Brian Cross <brian.cross.1977@gmail.com>
2021-09-30 23:25:58 -0600

    Created author component

---
+++ client/components/Author.js
@@ -0,0 +1,24 @@
+import Avatar from "./Avatar";
+
+export default function Author({ details }) {
+  const { avatar, name } = details;
+
+  return (
+    <>
+      {(avatar || name) && (
+        <div className="author">
+          <Avatar image={avatar} />
+          <span>{name}</span>
+        </div>
+      )}
+      <style jsx>{`
+        .author {
+          display: flex;
+          align-items: center;
+          gap: 1rem;
+          margin: 1rem 0;
+        }
+      `}</style>
+    </>
+  );
+}
