a34aa9f5a6bde15361eb316960e444a576f14203
Brian Cross <brian.cross.1977@gmail.com>
2021-09-30 19:44:51 -0600

    Update deployed site URL

--- studio/lib/resolveProductionUrl.js
+++ studio/lib/resolveProductionUrl.js
@@ -1,7 +1,7 @@
 const previewSecret =
   "UEW2eHklPn5x3KFcCqIs7sdk2ZGmShyJ6yex5KFeeXCtWWnLSt7HJWCqtJRiKuPr4x66oVqv3ZRTD3KWf66kAfHO8svvinDt01ccSL13GayxsLX0OvLmHa0udoWKz4qp";
 
-const remoteUrl = `https://your-studio.sanity.studio`;
+const remoteUrl = `https://nextjs-sanity-example-blog.vercel.app`;
 const localUrl = `http://localhost:3000`;
 
 export default function resolveProductionUrl(document) {
