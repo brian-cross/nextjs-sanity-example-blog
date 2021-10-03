bec7668d850442f4cc6589f8e3c1f19547609a05
Brian Cross <brian.cross.1977@gmail.com>
2021-10-02 20:48:45 -0600

    Removed unused code

--- studio/schemas/hero.js
+++ studio/schemas/hero.js
@@ -1,10 +1,7 @@
-// import { FaRegImage } from "react-icons/fa";
-
 export default {
   title: "Hero",
   name: "hero",
   type: "document",
-  // icon: FaRegImage,
   __experimental_actions: [/*'create',*/ "update", /*'delete',*/ "publish"],
   fields: [
     {
