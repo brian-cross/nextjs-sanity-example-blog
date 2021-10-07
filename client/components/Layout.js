615793a708d6146b8dba2b097076022bab271a70
Brian Cross <brian.cross.1977@gmail.com>
2021-10-06 22:44:41 -0600

    Switched to useRouter hook

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,13 +1,13 @@
-import { usePath } from "../lib/hooks";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 export default function Layout({ children, preview = false }) {
-  const path = usePath();
+  const router = useRouter();
 
   return (
     <>
       {preview && (
-        <a href={`/api/exitPreview/?slug=${path}`}>
+        <a href={`/api/exitPreview/?slug=${router.asPath}`}>
           Previewing. Click to exit.
         </a>
       )}
--- client/lib/hooks/index.js
+++ client/lib/hooks/index.js
@@ -1,3 +1,2 @@
-export { usePath } from "./usePath";
 export { usePreview } from "./usePreview";
 export { useRemToPx } from "./useRemToPx";
