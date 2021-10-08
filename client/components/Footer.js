e4140a0caa2997b6d1337f11148d59e404608210
Brian Cross <brian.cross.1977@gmail.com>
2021-10-07 21:16:57 -0600

    Changed content

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -7,13 +7,13 @@ export default function Footer() {
         <Container>
           <p>
             Powered by <a href="https://nextjs.org/">Next.js</a> and{" "}
-            <a href="https://www.sanity.io/">Sanity.io</a>
-          </p>
-          <p>
-            Source code is available on{" "}
+            <a href="https://www.sanity.io/">Sanity.io</a>. Source code on{" "}
             <a href="https://github.com/brian-cross/nextjs-sanity-example-blog">
               GitHub
             </a>
+          </p>
+          <p>
+            Made by <a href="https://www.briancross.ca">Brian</a>
           </p>
         </Container>
       </footer>
