b58064bd13a766b68158251ab42f9e4fc101c74c
Brian Cross <brian.cross.1977@gmail.com>
2021-10-11 20:55:14 -0600

    Moved blog specific documents into Blog folder

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -2,6 +2,10 @@ import S from "@sanity/desk-tool/structure-builder";
 import Iframe from "sanity-plugin-iframe-pane";
 import resolveProductionUrl from "./lib/resolveProductionUrl";
 import PanoramaRoundedIcon from "@mui/icons-material/PanoramaRounded";
+import ArticleRoundedIcon from "@mui/icons-material/ArticleRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import TagRoundedIcon from "@mui/icons-material/TagRounded";
+import FolderRoundedIcon from "@mui/icons-material/FolderRounded";
 
 export const getDefaultDocumentNode = ({ schemaType }) => {
   if (schemaType === "post")
@@ -27,6 +31,28 @@ export default () =>
           S.document().title("Hero").schemaType("hero").documentId("hero")
         ),
       ...S.documentTypeListItems().filter(
-        listItem => !["hero"].includes(listItem.getId())
+        listItem =>
+          !["hero", "post", "author", "tag"].includes(listItem.getId())
       ),
+      S.listItem()
+        .title("Blog")
+        .icon(FolderRoundedIcon)
+        .child(
+          S.list()
+            .title("Blog")
+            .items([
+              S.listItem()
+                .title("Posts")
+                .icon(ArticleRoundedIcon)
+                .child(S.documentTypeList("post").title("Blog Posts").child()),
+              S.listItem()
+                .title("Author")
+                .icon(PersonRoundedIcon)
+                .child(S.documentTypeList("author").title("Authors").child()),
+              S.listItem()
+                .title("Tags")
+                .icon(TagRoundedIcon)
+                .child(S.documentTypeList("tag").title("Tags").child()),
+            ])
+        ),
     ]);
