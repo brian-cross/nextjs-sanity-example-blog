fe08cce4cb6f482b1aa19831aa903cf1749df75d
Brian Cross <brian.cross.1977@gmail.com>
2021-10-11 20:53:33 -0600

    Added page schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,12 +1,13 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import hero from "./hero";
 import author from "./author";
+import hero from "./hero";
+import page from "./page";
 import post from "./post";
 import tag from "./tag";
 
 export default createSchema({
   name: "blogSchema",
-  types: schemaTypes.concat([hero, post, author, tag]),
+  types: schemaTypes.concat([hero, page, post, author, tag]),
 });
